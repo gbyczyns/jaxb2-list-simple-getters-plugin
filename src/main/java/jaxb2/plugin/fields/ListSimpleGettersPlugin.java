1cd57b5a9b9004a21ac7584c3dadfb37cdf0ceee
Grzegorz Byczyński <grzegorz.byczynski@comarch.com>
2018-04-04 09:26:47 +0200

    Removed customizations

--- src/main/java/jaxb2/plugin/fields/ListSimpleGettersPlugin.java
+++ src/main/java/jaxb2/plugin/fields/ListSimpleGettersPlugin.java
@@ -1,5 +1,12 @@
 package jaxb2.plugin.fields;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.jvnet.jaxb2_commons.plugin.AbstractParameterizablePlugin;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+
 import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JCodeModel;
@@ -7,33 +14,12 @@ import com.sun.codemodel.JFieldVar;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JMod;
 import com.sun.tools.xjc.Options;
-import com.sun.tools.xjc.model.CPluginCustomization;
 import com.sun.tools.xjc.outline.ClassOutline;
-import com.sun.tools.xjc.outline.FieldOutline;
 import com.sun.tools.xjc.outline.Outline;
-
-import org.jvnet.jaxb2_commons.plugin.AbstractParameterizablePlugin;
-import org.jvnet.jaxb2_commons.util.CustomizationUtils;
-import org.xml.sax.ErrorHandler;
-import org.xml.sax.SAXException;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.xml.namespace.QName;
 
 public class ListSimpleGettersPlugin extends AbstractParameterizablePlugin {
 
     private static final String OPTION_NAME = "Xlist-simple-getters";
-
-    private static final String NAMESPACE_URI = "http://list.simple.getters";
-
-    /**
-     * Tags that will be processed during plugin execution
-     */
-    public static final QName ATTR_CREATE_SIMPLE_GETTER_QNAME = new QName(NAMESPACE_URI, "create-simple-getter");
 
     @Override
     public String getOptionName() {
@@ -59,15 +45,7 @@ public class ListSimpleGettersPlugin extends AbstractParameterizablePlugin {
             Arrays.stream(classOutline.getDeclaredFields())
                     .filter(fieldOutline -> fieldOutline.getRawType().erasure() instanceof JClass)
                     .filter(fieldOutline -> refList.isAssignableFrom((JClass) fieldOutline.getRawType().erasure()))
-                    .forEach(fieldOutline ->
-                            getNamespaceCustomizations(fieldOutline)
-                                    .stream()
-                                    .filter(customization -> ATTR_CREATE_SIMPLE_GETTER_QNAME.getLocalPart().equals(customization.element.getLocalName()))
-                                    .forEach(customization -> {
-
-                                        //Mark that this customization was processed
-                                        customization.markAsAcknowledged();
-
+                    .forEach(fieldOutline -> {
                                         JFieldVar jFieldVar = classOutline.implClass.fields().get(fieldOutline.getPropertyInfo().getName(false));
 
                                         String getterName = createConventionGetterName(jFieldVar.name());
@@ -76,9 +54,7 @@ public class ListSimpleGettersPlugin extends AbstractParameterizablePlugin {
                                                 .removeIf(jMethod -> getterName.equals(jMethod.name()) && jMethod.params().isEmpty());
 
                                         createSimpleGetter(classOutline, jFieldVar, getterName);
-
-                                    })
-                    );
+                    });
         }
         return true;
     }
@@ -103,23 +79,4 @@ public class ListSimpleGettersPlugin extends AbstractParameterizablePlugin {
 
         return jMethod;
     }
-
-    /**
-     * Return all customizations from NAMESPACE_URI namespace
-     */
-    private List<CPluginCustomization> getNamespaceCustomizations(FieldOutline fieldOutline) {
-        return CustomizationUtils.getCustomizations(fieldOutline).stream()
-                .filter(cPluginCustomization ->
-                        NAMESPACE_URI.equals(cPluginCustomization.element.getNamespaceURI()))
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * Returns list of QNAMEs which we know how to handle
-     **/
-    @Override
-    public Collection<QName> getCustomizationElementNames() {
-        return Arrays.asList(ATTR_CREATE_SIMPLE_GETTER_QNAME);
-    }
-
 }
