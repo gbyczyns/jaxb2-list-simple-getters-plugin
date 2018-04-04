e2deb4cb3cca14ea736fa55466bb5ea4b81415e3
Grzegorz Byczyński <grzegorz.byczynski@comarch.com>
2018-04-04 09:36:12 +0200

    Code formatting

--- src/main/java/jaxb2/plugin/fields/ListSimpleGettersPlugin.java
+++ src/main/java/jaxb2/plugin/fields/ListSimpleGettersPlugin.java
@@ -19,61 +19,59 @@ import com.sun.tools.xjc.outline.Outline;
 
 public class ListSimpleGettersPlugin extends AbstractParameterizablePlugin {
 
-    private static final String OPTION_NAME = "Xlist-simple-getters";
+	private static final String OPTION_NAME = "Xlist-simple-getters";
 
-    @Override
-    public String getOptionName() {
-        return OPTION_NAME;
-    }
+	@Override
+	public String getOptionName() {
+		return OPTION_NAME;
+	}
 
-    @Override
-    public String getUsage() {
-        return "-" + OPTION_NAME + ": Generate simple getters for List<?> fields";
-    }
+	@Override
+	public String getUsage() {
+		return "-" + OPTION_NAME + ": Generate simple getters for List<?> fields";
+	}
 
-    /**
-     * Plugin entry-point
-     */
-    @Override
-    public boolean run(Outline outline, Options opt, ErrorHandler errorHandler)
-            throws SAXException {
+	/**
+	 * Plugin entry-point
+	 */
+	@Override
+	public boolean run(Outline outline, Options opt, ErrorHandler errorHandler) throws SAXException {
 
-        for (ClassOutline classOutline : outline.getClasses()) {
-            JCodeModel codeModel = classOutline.implClass.owner();
-            JClass refList = codeModel.ref(List.class);
+		for (ClassOutline classOutline : outline.getClasses()) {
+			JCodeModel codeModel = classOutline.implClass.owner();
+			JClass refList = codeModel.ref(List.class);
 
-            Arrays.stream(classOutline.getDeclaredFields())
-                    .filter(fieldOutline -> fieldOutline.getRawType().erasure() instanceof JClass)
-                    .filter(fieldOutline -> refList.isAssignableFrom((JClass) fieldOutline.getRawType().erasure()))
-                    .forEach(fieldOutline -> {
-                                        JFieldVar jFieldVar = classOutline.implClass.fields().get(fieldOutline.getPropertyInfo().getName(false));
+			Arrays.stream(classOutline.getDeclaredFields()) //
+				.filter(fieldOutline -> fieldOutline.getRawType().erasure() instanceof JClass) //
+				.filter(fieldOutline -> refList.isAssignableFrom((JClass) fieldOutline.getRawType().erasure())) //
+				.forEach(fieldOutline -> {
+					JFieldVar jFieldVar = classOutline.implClass.fields().get(fieldOutline.getPropertyInfo().getName(false));
 
-                                        String getterName = createConventionGetterName(jFieldVar.name());
+					String getterName = createConventionGetterName(jFieldVar.name());
 
-                                        classOutline.implClass.methods()
-                                                .removeIf(jMethod -> getterName.equals(jMethod.name()) && jMethod.params().isEmpty());
+					classOutline.implClass.methods().removeIf(jMethod -> getterName.equals(jMethod.name()) && jMethod.params().isEmpty());
 
-                                        createSimpleGetter(classOutline, jFieldVar, getterName);
-                    });
-        }
-        return true;
-    }
+					createSimpleGetter(classOutline, jFieldVar, getterName);
+				});
+		}
+		return true;
+	}
 
-    private String createConventionGetterName(String fieldName) {
-        char firstLetter = fieldName.charAt(0);
-        String firstLetterUpperCase = Character.valueOf(firstLetter).toString().toUpperCase();
-        return "get" + firstLetterUpperCase + fieldName.substring(1);
-    }
+	private String createConventionGetterName(String fieldName) {
+		char firstLetter = fieldName.charAt(0);
+		String firstLetterUpperCase = Character.valueOf(firstLetter).toString().toUpperCase();
+		return "get" + firstLetterUpperCase + fieldName.substring(1);
+	}
 
-    private JMethod createSimpleGetter(ClassOutline classOutline, JFieldVar jFieldVar, String mehtodName) {
-        JMethod jMethod = classOutline.implClass.method(JMod.PUBLIC, jFieldVar.type(), mehtodName);
+	private JMethod createSimpleGetter(ClassOutline classOutline, JFieldVar jFieldVar, String mehtodName) {
+		JMethod jMethod = classOutline.implClass.method(JMod.PUBLIC, jFieldVar.type(), mehtodName);
 
-        // Add method body
-        JBlock jBlock = jMethod.body();
+		// Add method body
+		JBlock jBlock = jMethod.body();
 
-        // return variable
-        jBlock._return(jFieldVar);
+		// return variable
+		jBlock._return(jFieldVar);
 
-        return jMethod;
-    }
+		return jMethod;
+	}
 }
